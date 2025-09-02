17d718df7630f1b7a56c1ce9397508a0170b0e30
Ehan A <ehan@quizfreely.org>
2025-09-02 15:37:41 -0400

    new table

---
+++ db/migrations/202509021427_term_progress_history.sql
@@ -0,0 +1,13 @@
+-- migrate:up
+create table term_progress_history (
+    id uuid primary key default gen_random_uuid(),
+    timestamp timestamptz not null,
+    term_id uuid not null references terms(id),
+    user_id uuid not null references users(id),
+    term_correct_count int,
+    term_incorrect_count int,
+    def_correct_count int,
+    def_incorrect_count int
+);
+
+-- migrate:down
