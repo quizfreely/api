5e77ff9ab539a19de4d97816ba1264a2c5c02d8d
Ehan <ehanahamed@ehan.dev>
2025-09-03 09:12:04 -0400

    yea we dont need those

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -812,38 +812,3 @@ type queryResolver struct{ *Resolver }
 type studysetResolver struct{ *Resolver }
 type termResolver struct{ *Resolver }
 type termConfusionPairResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *termProgressResolver) TermFirstReviewedAt(ctx context.Context, obj *model.TermProgress) (*string, error) {
-	panic(fmt.Errorf("not implemented: TermFirstReviewedAt - termFirstReviewedAt"))
-}
-func (r *termProgressResolver) TermLastReviewedAt(ctx context.Context, obj *model.TermProgress) (*string, error) {
-	panic(fmt.Errorf("not implemented: TermLastReviewedAt - termLastReviewedAt"))
-}
-func (r *termProgressResolver) TermReviewCount(ctx context.Context, obj *model.TermProgress) (*int32, error) {
-	panic(fmt.Errorf("not implemented: TermReviewCount - termReviewCount"))
-}
-func (r *termProgressResolver) DefFirstReviewedAt(ctx context.Context, obj *model.TermProgress) (*string, error) {
-	panic(fmt.Errorf("not implemented: DefFirstReviewedAt - defFirstReviewedAt"))
-}
-func (r *termProgressResolver) DefLastReviewedAt(ctx context.Context, obj *model.TermProgress) (*string, error) {
-	panic(fmt.Errorf("not implemented: DefLastReviewedAt - defLastReviewedAt"))
-}
-func (r *termProgressResolver) DefReviewCount(ctx context.Context, obj *model.TermProgress) (*int32, error) {
-	panic(fmt.Errorf("not implemented: DefReviewCount - defReviewCount"))
-}
-func (r *termProgressResolver) TermLeitnerSystemBox(ctx context.Context, obj *model.TermProgress) (*int32, error) {
-	panic(fmt.Errorf("not implemented: TermLeitnerSystemBox - termLeitnerSystemBox"))
-}
-func (r *termProgressResolver) DefLeitnerSystemBox(ctx context.Context, obj *model.TermProgress) (*int32, error) {
-	panic(fmt.Errorf("not implemented: DefLeitnerSystemBox - defLeitnerSystemBox"))
-}
-func (r *Resolver) TermProgress() TermProgressResolver { return &termProgressResolver{r} }
-type termProgressResolver struct{ *Resolver }
-*/
