e0e9c5822db17bd73b627c3701db8b6e6906793d
Ehan <ehanahamed@ehan.dev>
2025-09-02 21:01:46 -0400

    fix

--- db/migrations/202509021427_term_progress_history.sql
+++ db/migrations/202509021427_term_progress_history.sql
@@ -3,7 +3,7 @@ create table term_progress_history (
     id uuid primary key default gen_random_uuid(),
     timestamp timestamptz not null default now(),
     term_id uuid not null references terms(id),
-    user_id uuid not null references users(id),
+    user_id uuid not null references auth.users(id),
     term_correct_count int,
     term_incorrect_count int,
     def_correct_count int,
