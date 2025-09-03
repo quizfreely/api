c6033dd27ad12ec48ac0b7a308e0dc7a151e7892
Ehan <ehanahamed@ehan.dev>
2025-09-02 23:01:47 -0400

    begin implementing dataloader & also add missing timestamp for struct

--- graph/model/progress_history.go
+++ graph/model/progress_history.go
@@ -3,6 +3,7 @@ package model
 type TermProgress struct {
 	ID                   *string `json:"id,omitempty"`
 	TermID                   *string `json:"termId,omitempty"`
+	Timestamp            *string     `json:"timestamp,omitempty"`
 	TermCorrectCount     *int32  `json:"termCorrectCount,omitempty"`
 	TermIncorrectCount   *int32  `json:"termIncorrectCount,omitempty"`
 	DefCorrectCount      *int32  `json:"defCorrectCount,omitempty"`
