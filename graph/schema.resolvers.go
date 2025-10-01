dafd3c0777fefb6154aaca4c82deecc1e136405c
Ehan <ehanahamed@ehan.dev>
2025-10-01 00:36:11 -0400

    adfadffasd

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -499,7 +499,7 @@ RETURNING
 // CreateFeaturedCategory is the resolver for the createFeaturedCategory field.
 func (r *mutationResolver) CreateFeaturedCategory(ctx context.Context, title *string) (*model.Category, error) {
 	authedUser := auth.AuthedUserContext(ctx)
-	if authedUser == nil || authedUser.ModPerms != true {
+	if authedUser == nil || *authedUser.ModPerms != true {
 		return nil, fmt.Errorf("only mods are allowed to create featured categories")
 	}
 
@@ -507,7 +507,7 @@ func (r *mutationResolver) CreateFeaturedCategory(ctx context.Context, title *st
 	err := pgxscan.Get(
 		ctx,
 		r.DB,
-		&practiceTest,
+		&category,
 		`INSERT INTO featured_categories
 (title) VALUES ($1)
 RETURNING id, title`,
@@ -523,7 +523,7 @@ RETURNING id, title`,
 // SetStudysetFeaturedCategory is the resolver for the setStudysetFeaturedCategory field.
 func (r *mutationResolver) SetStudysetFeaturedCategory(ctx context.Context, studysetID *string, categoryID *string) (*bool, error) {
 	authedUser := auth.AuthedUserContext(ctx)
-	if authedUser == nil || authedUser.ModPerms != true {
+	if authedUser == nil || *authedUser.ModPerms != true {
 		return nil, fmt.Errorf("only mods are allowed to set featured categories")
 	}
 
