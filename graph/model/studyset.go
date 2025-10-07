2a5f0492fd71388b956395ae12c832830feb109b
Ehan A <ehan@quizfreely.org>
2025-10-06 23:29:06 -0400

    update studyset model

--- graph/model/studyset.go
+++ graph/model/studyset.go
@@ -4,6 +4,7 @@ type Studyset struct {
 	ID        *string `json:"id,omitempty"`
 	Title     *string `json:"title,omitempty"`
 	Private   *bool   `json:"private,omitempty"`
+	SubjectID *string  `json:"subjectId,omitempty"`
 	UpdatedAt *string `json:"updatedAt,omitempty"`
 	UserID      *string   `json:"userId,omitempty"`
 	User      *User   `json:"user,omitempty"`
