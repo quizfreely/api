6a961b6bdff91986d86b5e6d8d1e8df4505cd5b4
Ehan <ehanahamed@ehan.dev>
2025-10-07 18:57:06 -0400

    enforce uniqueness

--- db/migrations/202510071846_fix_saved_studysets.sql
+++ db/migrations/202510071846_fix_saved_studysets.sql
@@ -9,4 +9,7 @@ grant insert on folders to quizfreely_api;
 grant update on folders to quizfreely_api;
 grant delete on folders to quizfreely_api;
 
+ALTER TABLE saved_studysets
+ADD CONSTRAINT saved_studysets_user_studyset_unique UNIQUE (user_id, studyset_id);
+
 -- migrate:down
