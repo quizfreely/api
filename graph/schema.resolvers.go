35a06dfe8a6ba76ce94f1159977c30181c4aa109
Ehan <ehanahamed@ehan.dev>
2025-10-30 21:01:24 -0400

    so basically thx

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -1404,14 +1404,3 @@ type studysetResolver struct{ *Resolver }
 type subjectResolver struct{ *Resolver }
 type termResolver struct{ *Resolver }
 type termConfusionPairResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *queryResolver) RecentStudysets(ctx context.Context, limit *int32, offset *int32) ([]*model.Studyset, error) {
-}
-*/
