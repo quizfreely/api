f728e413029c7caaac738a8be5d5a06a608a5ccf
Ehan A <ehan@quizfreely.org>
2026-03-21 21:49:18 -0400

    missing imports, also lets log errors

--- rest/health.go
+++ rest/health.go
@@ -1,5 +1,10 @@
 package rest
 
+import (
+	"net/http"
+	"github.com/rs/zerolog/log"
+)
+
 func (rh *RESTHandler) Health(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	err := rh.DB.Ping(ctx)
@@ -8,5 +13,6 @@ func (rh *RESTHandler) Health(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.WriteHeader(503)
 		w.Write([]byte("503 Service Unavailable: DB error, API running"))
+		log.Error().Err(err).Msg("DB error from /health")
 	}
 }
