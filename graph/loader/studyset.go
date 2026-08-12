ad26490243708b85b3f766c2a33a811abce989f8
Ehan A <ehan@quizfreely.org>
2026-08-12 00:29:57 -0400

    fix errs in dataloader

--- graph/loader/studyset.go
+++ graph/loader/studyset.go
@@ -2,6 +2,7 @@ package loader
 
 import (
 	"context"
+	"fmt"
 	"quizfreely/api/auth"
 	"quizfreely/api/graph/model"
 
@@ -31,7 +32,7 @@ func (dr *dataReader) getStudysetsByIDs(ctx context.Context, ids []string) ([]*m
 	var err error
 
 	if authedUser != nil {
-		err = pgxscan.Select(ctx, r.DB, &rows, `
+		err = pgxscan.Select(ctx, dr.DB, &rows, `
 			SELECT `+selectCols+`
 			FROM unnest($1::uuid[]) WITH ORDINALITY AS input(id, ordinality)
 			LEFT JOIN studysets s ON s.id = input.id
@@ -40,7 +41,7 @@ func (dr *dataReader) getStudysetsByIDs(ctx context.Context, ids []string) ([]*m
 			ORDER BY input.ordinality
 		`, ids, authedUser.ID)
 	} else {
-		err = pgxscan.Select(ctx, r.DB, &rows, `
+		err = pgxscan.Select(ctx, dr.DB, &rows, `
 			SELECT `+selectCols+`
 			FROM unnest($1::uuid[]) WITH ORDINALITY AS input(id, ordinality)
 			LEFT JOIN studysets s ON s.id = input.id
